949699dfd8f3fe8e8d267b27ef2cb5b234b221eb
bindu21 <bindu21india@gmail.com>
2018-12-02 16:32:58 -0500

    changes and adding more functionality for different scenarios

--- main_file.py
+++ main_file.py
@@ -1,24 +1,43 @@
 import json
+import difflib
+from difflib import get_close_matches
+my_data = json.load(open("data.json"))
+def give_me_def(word):
+	for i in my_data[word]:
+		print(i)
+	
 def main_fun():
 	flag = False
-	my_data = json.load(open("data.json"))
+	
 	while(1):
 		p = input('Press "Y" to continue and "N" to exit \n')
 		if "n" == p.lower():
 			exit()
 		elif "y" == p.lower():
 			st = input("Enter the word you want \n")
-			for key,value in my_data.items():
-				if st == key:
-					for i in value:
-						print(i)
-					flag=True
-					break
-			if flag == True:
-				continue
+			st=st.lower()
+			if st in my_data:
+				give_me_def(st)
+				flag=True
 			else:
-				print("You entered somthing wrong.. Please try again")
-				continue
+				new_word = get_close_matches(st,my_data.keys(),cutoff=0.7)
+				if new_word:
+					print("Did you mean {}".format(new_word[0]))
+					take=input('Press "Y" if yes')
+					if "y"==take.lower():
+						st = new_word[0]
+						give_me_def(st)
+						flag=True
+					else:
+						continue
+				else:
+					flag = False
+			
+		if flag == True:
+			continue
+		else:
+			print("You entered somthing wrong.. Please try again")
+			continue
 				
 if __name__ == "__main__":
 		main_fun()
